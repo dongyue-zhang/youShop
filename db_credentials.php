63ad5cc35d3be505d3a2d619f9d08037c0173bd6
Dongyue Zhang <zhangdongyue22@gmail.com>
2022-08-28 01:45:38 -0400

    fixed DB_PASS var name

--- db_credentials.php
+++ db_credentials.php
@@ -2,7 +2,7 @@
 
 define("DB_SERVER", $_ENV['DB_SERVER']);
 define("DB_USER", $_ENV['DB_USER']);
-define("DB_PASS", $_ENV['DA_PASS']);
+define("DB_PASS", $_ENV['DB_PASS']);
 define("DB_NAME", $_ENV['DB_NAME']);
 
 ?>
