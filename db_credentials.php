ce3505b639836e530e31c28086860234f41cf501
Dongyue Zhang <zhangdongyue22@gmail.com>
2022-08-28 01:24:02 -0400

    changed getenv

--- db_credentials.php
+++ db_credentials.php
@@ -2,7 +2,7 @@
 
 define("DB_SERVER", getenv('DB_SERVER'));
 define("DB_USER", getenv('DB_USER'));
-define("DB_PASS", getenv("DA_PASS"));
+define("DB_PASS", getenv('DA_PASS'));
 define("DB_NAME", getenv('DB_NAME'));
 
 ?>
